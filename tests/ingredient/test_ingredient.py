6114ee4df8753d6b166028e6decda3f1757771d3
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-06-23 12:09:07 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    presunto = Ingredient("presunto")
+    salmão = Ingredient("salmão")
+
+    assert presunto.name == "presunto"
+    assert presunto.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert presunto.__repr__() == "Ingredient('presunto')"
+
+    assert presunto.__eq__(salmão) is False
+    assert presunto.__eq__(presunto) is True
+
+    assert presunto.__hash__() == presunto.__hash__()
+    assert presunto.__hash__() != salmão.__hash__()
